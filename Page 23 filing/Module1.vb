c556697bbef03ed7141b686a90114b31034e64ef
Taha Anwar <123452083+tahaanwar31@users.noreply.github.com>
2023-01-24 16:10:23 +0500

    Alevels 9618 booklet page 23 filing qs

---
+++ Page 23 filing/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Sub Main()
+        Preview()
+    End Sub
+    Sub Preview()
+        Dim data As String
+        Dim count As Integer
+        data = ""
+        count = 0
+        FileOpen(1, My.Application.Info.DirectoryPath & "\textfile.txt", OpenMode.Input)
+        If EOF(1) Then
+            Console.Writeline("File is empty")
+        Else
+            While Not EOF(1) And count <= 5
+                Input(1, data)
+                Console.WriteLine(data)
+                count = count + 1
+            End While
+        End If
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+End Module
